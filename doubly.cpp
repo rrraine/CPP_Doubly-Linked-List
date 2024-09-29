d5604f7246e1c9ea7dd02df7460d98f6742c7014
rrraine <lorraineequezada@gmail.com>
2024-09-29 20:12:32 +0800

    Update doubly.cpp

--- doubly.cpp
+++ doubly.cpp
@@ -13,28 +13,53 @@ class Doubly : public List {
 
     // helper functions
     void addBetween(node* curr, node* node){
-
+        return;
     }
 
     int removeBetween(node* curr){
-
+        return 0;
     }
 
     node* traverseTail(int pos){
-
+        return NULL;
     }
 
     node* traverseHead(int pos){
-
+        return NULL;
     }
     public:
 
     void addHead(int num) {
-        
+        node* newNode = new node(num);
+        if (head == NULL){
+            head = newNode;
+            tail = newNode;
+            size++;
+            return;
+        }
+
+        newNode->next = head;
+        newNode->prev = NULL;
+        head->prev = newNode;
+        head = newNode;
+        size++;
+        return;
     }
 
     void addTail(int num) {
+        node* newNode = new node(num);
+        if (tail == NULL){
+           head = newNode;
+           tail = newNode;
+           size++;
+           return;
+        }
 
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+        size++;
+        return;
     }
 
     void addAt(int num, int pos) {
@@ -42,42 +67,40 @@ class Doubly : public List {
     }
 
     int removeHead(){
-
+        return 0;
     }
 
     int removeTail(){
-
-
+        return 0;
     }   
+
     int removeAt(int pos){
-
-
+        return 0;
     }  
+
     int removeAll(int num) {
-
-
+        return 0;
     }  
 
 
     int get(int pos) {
-
-
+        return 0;
     }  
 
     int retain(int num) {
-        
+        return 0;
     }  
 
     int corner(int left, int right) {
-
+        return 0;
     }  
 
     void reverse(){
-
+        return;
     }   
 
     void print(){
-
+        return;
     }
 
 };
